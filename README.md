0c30cd91cf0e582889a918b7192c006975a37e09
Øyvind Nilsen <oyvind@oan.lan>
2024-03-25 10:03:10 +0100

    seems to be working

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # last2toggle
+Chrome Extension to toggle between last two tabs.
+
+## Default Hotkey
+```
+Control + Space
+```
--- background.js
+++ background.js
@@ -1,8 +1,16 @@
+let tabStack = [];
 
-chrome.commands.onCommand.addListener((command,tab) => {
-    if (command === 'run_foo') {
-        console.log(`Command: ${command}`);
-    } else {
-        console.log(`Command: ${command}`);
-    }    
+chrome.tabs.onActivated.addListener(activeInfo => {
+  tabStack.push(activeInfo.tabId);
+  if (tabStack.length > 2) {
+    tabStack.shift();
+  }
+  //console.log("Logging tab");
+});
+
+chrome.commands.onCommand.addListener(command => {
+  if (command === "toggle_tabs" && tabStack.length === 2) {
+    chrome.tabs.update(tabStack[0], { active: true });
+    //console.log("Toggling tabs");
+  }
 });
